ede04f324353102277ccd04adbb8e0a892e86e38
systemTetra2 <ali.raza03086674345@gmail.com>
2023-02-06 04:50:37 +0500

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-    //
+      $this->app['request']->server->set('HTTPS', true);
     }
 
     /**
@@ -21,8 +22,9 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
       \URL::forceScheme('https');
+      $url->formatScheme('https');
     }
 }
