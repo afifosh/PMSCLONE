414829d2b1891714797d23fa96b816911dddfc00
systemTetra2 <ali.raza03086674345@gmail.com>
2023-02-10 12:21:08 +0500

    queue worker

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('queue:work --tries=3 --max-time=300 --stop-when-empty')
+          ->everyMinute()
+          ->withoutOverlapping();
     }
 
     /**
