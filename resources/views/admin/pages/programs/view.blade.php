aab58e56b53e0a338a441ea20bfce3b1f1807af1
systemTetra2 <ali.raza03086674345@gmail.com>
2023-02-10 15:52:42 +0500

    t

--- resources/views/admin/pages/programs/view.blade.php
+++ resources/views/admin/pages/programs/view.blade.php
@@ -1,6 +1,6 @@
 @extends('admin/layouts/layoutMaster')
 
-@section('title', 'User Profile - Profile')
+@section('title', $program->name.' - Program')
 
 @section('vendor-style')
 <link rel="stylesheet" href="{{asset('assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css')}}">
@@ -23,9 +23,9 @@
 @endsection
 
 @section('content')
-<h4 class="fw-bold py-3 mb-4">
+{{-- <h4 class="fw-bold py-3 mb-4">
   <span class="text-muted fw-light">User Profile /</span> Profile
-</h4>
+</h4> --}}
 
 
 <!-- Header -->
