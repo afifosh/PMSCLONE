c6f25519492765d726b96a961ad2bbc915d78c68
Soban Farooq <sobanfarooq3@gmail.com>
2023-02-18 18:54:51 +0500

    refactored login response class

--- app/Http/Responses/LoginResponse.php
+++ app/Http/Responses/LoginResponse.php
@@ -2,25 +2,35 @@
 
 namespace App\Http\Responses;
 
-use App\Notifications\AuthLogNotification;
+use App\Notifications\Auth\AuthLogNotification;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
 use Laravel\Fortify\Http\Responses\LoginResponse as ResponsesLoginResponse;
-use Stevebauman\Location\Facades\Location;
 
 class LoginResponse extends ResponsesLoginResponse implements LoginResponseContract
 {
 
+  /**
+   * @param  $request
+   * @return mixed
+   */
+  public function toResponse($request)
+  {
     /**
-     * @param  $request
-     * @return mixed
+     * @var \App\Models\User
      */
-    public function toResponse($request)
-    {
-      $user = Auth::guard(config('fortify.guard'))->user();
-      if ($user->checkIfLastLoginDetailsChanged()) {
-        $user->notify(new AuthLogNotification($user->authentications, $user->lastLoginAgent(),  $user->lastLoginLocation()));
-      }
-      return parent::toResponse($request);
+    $user = Auth::guard(config('fortify.guard'))->user();
+
+    if ($user->findIfLoginUpdated()) {
+      $user->notify(
+        new AuthLogNotification(
+          $user->authentications,
+          $user->lastLoginAgent(),
+          $user->lastLoginLocation()
+        )
+      );
     }
+
+    return parent::toResponse($request);
+  }
 }
