6f24f11cabc0bfc4cf7529afe4dc3e8c2851be20
Soban Farooq <sobanfarooq3@gmail.com>
2023-02-21 00:38:27 +0500

    seconds correction in general settings

--- public/assets/js/custom/session-timeout.js
+++ public/assets/js/custom/session-timeout.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
+    const SECOND_CONVERSION = 1000;
     $.sessionTimeout({
         keepAliveUrl,
         logoutUrl,
         redirUrl,
-        warnAfter,
-        redirAfter,
+        warnAfter: warnAfter * SECOND_CONVERSION,
+        redirAfter: redirAfter * SECOND_CONVERSION,
         countdownBar: true,
         countdownMessage: 'Redirecting in {timer} seconds.',
         useLocalStorageSynchronization: true,
