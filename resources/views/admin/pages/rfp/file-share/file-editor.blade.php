98bfec8b176e38ecdc5e6b4d871c119ec268f8ec
systemTetra2 <ali.raza03086674345@gmail.com>
2023-02-25 17:24:18 +0500

    file share datatable bug

--- resources/views/admin/pages/rfp/file-share/file-editor.blade.php
+++ resources/views/admin/pages/rfp/file-share/file-editor.blade.php
@@ -1,5 +1,5 @@
 @if ($row->file->is_editable())
-    <a href="{{ route('admin.edit-file',[ 'file' => $row->file_id, 'rfp' => $row->file->rfp_id]) }}">{{ $row->file->title }}</a>
+    <a href="{{ route('admin.edit-file',[ 'file' => $row->rfp_file_id, 'rfp' => $row->file->rfp_id]) }}">{{ $row->file->title }}</a>
 @else
     {{ $row->file->title }}
 @endif
