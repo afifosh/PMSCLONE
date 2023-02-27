c0cdeedb6088789d73e4679c7b06435ca0db0e25
systemTetra2 <ali.raza03086674345@gmail.com>
2023-02-28 01:46:00 +0500

    test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,12 +19,12 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UserSeeder::class);
         // \App\Models\User::factory(30)->create();
-        $this->call(ModuleSeeder::class);
-        $this->call(AdminRolesPermissionsSeeder::class);
-        $this->call(UserRolesPermissionsSeeder::class);
-        $this->call(AdminSeeder::class);
-        $this->call(HRSeeder::class);
-        $this->call(CompanySeeder::class);
+        // $this->call(ModuleSeeder::class);
+        // $this->call(AdminRolesPermissionsSeeder::class);
+        // $this->call(UserRolesPermissionsSeeder::class);
+        // $this->call(AdminSeeder::class);
+        // $this->call(HRSeeder::class);
+        // $this->call(CompanySeeder::class);
         // $this->call(ProgramSeeder::class);
     }
 }
