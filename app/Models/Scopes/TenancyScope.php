754bfd921054fd9cceb05f310fd051d8b207e701
systemTetra2 <ali.raza03086674345@gmail.com>
2023-02-28 01:23:04 +0500

    global query scope fix

---
+++ app/Models/Scopes/TenancyScope.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Support\Facades\Auth;
+
+class TenancyScope implements Scope
+{
+  /**
+   * Apply the scope to a given Eloquent query builder.
+   *
+   * @param  \Illuminate\Database\Eloquent\Builder  $builder
+   * @param  \Illuminate\Database\Eloquent\Model  $model
+   * @return void
+   */
+  public function apply(Builder $builder, Model $model)
+  {
+    if (Auth::hasUser() && auth('web')->check()) {
+      return $builder->where('company_id', auth('web')->user()->company_id);
+    }
+  }
+}
--- app/Traits/Tenantable.php
+++ app/Traits/Tenantable.php
@@ -1,21 +1,22 @@
 <?php
+
 namespace App\Traits;
 
 use Illuminate\Database\Eloquent\Builder;
 
-trait Tenantable {
-
-    public static function bootTenantable() {
+trait Tenantable
+{
+  public static function bootTenantable()
+  {
+    if (auth('web')->check()) {
+      static::creating(function ($model) {
+        $model->company_id = auth('web')->user()->company_id;
+      });
+      static::addGlobalScope('company_id', function (Builder $builder) {
         if (auth('web')->check()) {
-            static::creating(function ($model) {
-                $model->company_id = auth('web')->user()->company_id;
-            });
-            static::addGlobalScope('company_id', function (Builder $builder) {
-                if (auth('web')->check()) {
-                    return $builder->where('company_id', auth('web')->user()->company_id);
-                }
-            });
+          return $builder->where('company_id', auth('web')->user()->company_id);
         }
+      });
     }
-
+  }
 }
