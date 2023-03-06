d19ff2b77ce7e7e9c02506c76c6b49951994ce07
afifosh <afif@live.com>
2023-03-06 22:46:23 +0300

    ignore public assets

--- public/assets/js/custom/app-settings.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    (function () {
-        $('#supportedMailServices').on('change', function () {
-            let selectedEmailService = $(this).val() + 'EmailService'
-            $('.email-service').addClass('d-none')
-            $('#' + selectedEmailService).removeClass('d-none')
-        })
-    })();
-})
--- public/assets/js/custom/flatpickr.js
+++
@@ -1,5 +0,0 @@
-window.initFlatPickr = function () {
-  $(".flatpickr").each(function (index, element) {
-    $(this).flatpickr($(this).data('flatpickr'));
-  });
-};
