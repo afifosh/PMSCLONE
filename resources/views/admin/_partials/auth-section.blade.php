9b870a024f2cc17e75e760506660f47b64bbe72b
afifosh <afif@live.com>
2023-03-07 10:21:35 +0300

    remove inline css style from auth-seciton

--- resources/views/admin/_partials/auth-section.blade.php
+++ resources/views/admin/_partials/auth-section.blade.php
@@ -1,9 +1,8 @@
-<div class="auth-one-bg-position auth-one-bg" id="auth-particles" style="background-image: url(https://www.rcrc.gov.sa/Tuwaiq-Sculpture/En/images/bg-white.png);background-position: center;background-size: cover;position: absolute;top: 0;left: 0;right: 0;width: 100%;height: 380px;">
-            <div class="bg-overlay" style="background: -webkit-gradient(linear,left top,right top,from(#364574),to(#405189));background: linear-gradient(to right,#000,#000);opacity: .9;position: absolute;height: 100%;width: 100%;right: 0;bottom: 0;left: 0;top: 0;opacity: .7;/*! background-color: #000; */"></div>
-
-            <div class="shape" style="position: absolute;bottom: 0;right: 0;left: 0;z-index: 1;pointer-events: none;">
-                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1440 120" style="width: 100%;height: auto;fill: #F8F7FA;">
-                    <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
-                </svg>
-            </div>
-        </div>
+<div class="auth-one-bg-position auth-one-bg" id="auth-particles" >
+    <div class="bg-overlay"></div>
+    <div class="shape" style="position: absolute;bottom: 0;right: 0;left: 0;z-index: 1;pointer-events: none;">
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1440 120" style="width: 100%;height: auto;fill: #F8F7FA;">
+          <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
+        </svg>
+    </div>
+</div>
