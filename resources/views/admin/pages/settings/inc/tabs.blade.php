84c0de5ad08fb8ceb9316a948352d9d1cb8ed3ae
Soban Farooq <sobanfarooq3@gmail.com>
2023-03-11 16:48:39 +0500

    renamed general to security and added headings for tabs

--- resources/views/admin/pages/settings/inc/tabs.blade.php
+++ resources/views/admin/pages/settings/inc/tabs.blade.php
@@ -1,19 +1,19 @@
 <!-- Settings Sidebar -->
 @php
-    $urls = [
-        'general' => 'admin.setting.index',
-        'email' => 'admin.setting.email',
-    ];
-    $active = Request::route()->getName();
+$urls = [
+'security' => 'admin.setting.index',
+'email' => 'admin.setting.email',
+];
+$active = Request::route()->getName();
 @endphp
 <div class="col setting-sidebar border-end flex-grow-0" id="setting-sidebar">
     <div class="btn-compost-wrapper d-grid"></div>
     <div class="setting-filter py-2">
         <ul class="setting-list list-unstyled mb-4">
-            <li class="{{ $active === $urls['general'] ? 'active' : '' }} d-flex justify-content-between py-3" data-target="general">
-                <a href="{{ route($urls['general']) }}" class="d-flex flex-wrap align-items-center">
-                    <i class="ti ti-mail"></i>
-                    <span class="align-middle ms-2">@lang('General')</span>
+            <li class="{{ $active === $urls['security'] ? 'active' : '' }} d-flex justify-content-between py-3" data-target="general">
+                <a href="{{ route($urls['security']) }}" class="d-flex flex-wrap align-items-center">
+                    <i class="ti ti-lock"></i>
+                    <span class="align-middle ms-2">@lang('Security')</span>
                 </a>
             </li>
             <li class="{{ $active === $urls['email'] ? 'active' : '' }} d-flex py-3" data-target="email">
