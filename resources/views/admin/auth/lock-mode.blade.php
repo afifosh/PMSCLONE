dfbdb1ede9276a99b8dcf9416d8b59708c2b6be9
afifosh <afif@live.com>
2023-03-12 12:32:08 +0300

    fix lock mode layout for admin and web

--- resources/views/admin/auth/lock-mode.blade.php
+++ resources/views/admin/auth/lock-mode.blade.php
@@ -52,7 +52,7 @@ $configData = Helper::appClasses();
                         <p class="mb-4">Enter your  password</p>
                         <div class="text-center">
                         <div class="mt-4">
-                              <div class="mx-auto d-block mb-3 lock-screen-avatar">
+                              <div class="mx-auto d-block mb-3">
                                 <img src="{{ Auth::user()->avatar }}" alt class="h-auto rounded-circle">
                            </div>
                          </div>
@@ -90,72 +90,4 @@ $configData = Helper::appClasses();
             </div>
         </div>
     </div>
-@endsection
-
-
-@section('content')
-<div class="authentication-wrapper authentication-cover authentication-bg">
-    <div class="authentication-inner row">
-        <!-- /Left Text -->
-        <div class="d-none d-lg-flex col-lg-7 p-0">
-            <div class="auth-cover-bg auth-cover-bg-color d-flex justify-content-center align-items-center">
-                <img src="{{ asset('assets/img/illustrations/auth-login-illustration-'.$configData['style'].'.png') }}" alt="auth-login-cover" class="img-fluid my-5 auth-illustration" data-app-light-img="illustrations/auth-login-illustration-light.png" data-app-dark-img="illustrations/auth-login-illustration-dark.png">
-
-                <img src="{{ asset('assets/img/illustrations/bg-shape-image-'.$configData['style'].'.png') }}" alt="auth-login-cover" class="platform-bg" data-app-light-img="illustrations/bg-shape-image-light.png" data-app-dark-img="illustrations/bg-shape-image-dark.png">
-            </div>
-        </div>
-        <!-- /Left Text -->
-
-        <!-- Login -->
-        <div class="d-flex col-12 col-lg-5 align-items-center p-sm-5 p-4">
-            <div class="w-px-400 mx-auto">
-                <!-- Logo -->
-                <div class="app-brand mb-4 d-flex flex-column align-items-center justify-content-center">
-                    <a href="{{url('/')}}" class="app-brand-link gap-2">
-                        <span class="app-brand-logo demo">@include('_partials.macros',["height"=>120,"withbg"=>'fill: #fff;'])</span>
-                    </a>
-                    <div class="mt-3">
-                        <h5 class="mb-1 fs-5 fw-bold text-uppercase">{{ __('Account Locked') }}</h5>
-                    </div>
-
-                    <div class="mt-4">
-                        <div class="avatar mb-3 lock-screen-avatar">
-                            <img src="{{ Auth::user()->avatar }}" alt class="h-auto rounded-circle">
-                        </div>
-                    </div>
-
-                    <div class="mt-5">
-                        <h5 class="fw-bold fs-6">{{ Auth::user()->full_name }}</h5>
-                    </div>
-                </div>
-
-                <form id="formAuthentication" class="mb-3" action="{{ route('admin.auth.unlock') }}" method="POST">
-                    @csrf
-                    @if ($errors->any())
-                    <div class="alert alert-danger alert-dismissible">
-                        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-                        {!! implode('<br />', $errors->all('<span>:message</span>')) !!}
-                    </div>
-                    @endif
-
-                    <div class="mb-4 form-password-toggle">
-                        <div class="input-group input-group-merge mb-1">
-                            <input type="password" id="password" class="form-control" name="password" placeholder="@lang('Password..')" aria-describedby="password" />
-                            <span class="input-group-text cursor-pointer"><i class="ti ti-eye-off"></i></span>
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <button class="btn btn-primary d-grid w-100" type="submit">
-                            <span class="d-flex align-items-center align-middle">
-                                <i class="ti ti-key me-2 ti-sm"></i>
-                                <span class="flex-grow-1 align-middle">{{ __('Unlock') }}</span>
-                            </span>
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-        <!-- /Login -->
-    </div>
-</div>
 @endsection
