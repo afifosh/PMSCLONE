9813ac888f8c9189267233372fbf16cab46f4427
afifosh <afif@live.com>
2023-03-12 10:09:14 +0300

    add new svg and auth section for default guard

---
+++ resources/views/_partials/auth-section.blade.php
@@ -0,0 +1,8 @@
+<div class="auth-one-bg-position auth-one-bg" id="auth-particles" >
+    <div class="bg-overlay"></div>
+    <div class="shape" style="position: absolute;bottom: 0;right: 0;left: 0;z-index: 1;pointer-events: none;">
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1440 120" style="width: 100%;height: auto;fill: #F8F7FA;">
+          <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
+        </svg>
+    </div>
+</div>
