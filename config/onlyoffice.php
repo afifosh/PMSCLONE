5afff6c451379decaadefa9cd545a8f599d8906e
systemTetra2 <ali.raza03086674345@gmail.com>
2023-03-15 17:53:11 +0500

    onlyOffice credentials update

--- config/onlyoffice.php
+++ config/onlyoffice.php
@@ -5,8 +5,8 @@ return [
    * secret
    */
   "secret" => env('ONLYOFFICE_SECRET', null),
-  "doc_server_url" => 'http://146.190.123.183',
-  "doc_server_api_url" => 'http://146.190.123.183/web-apps/apps/api/documents/api.js',
+  "doc_server_url" => env('ONLYOFFICE_HOST_URL', 'http://167.71.55.200'),
+  "doc_server_api_url" => env('ONLYOFFICE_HOST_API_URL', 'http://167.71.55.200/web-apps/apps/api/documents/api.js'),
 
   'supported_files' => [
     'csv', 'djvu', 'doc', 'docm', 'docx', 'docxf', 'dot', 'dotm', 'dotx', 'epub', 'fb2', 'fodp', 'fods', 'fodt', 'htm', 'html', 'mht', 'odp', 'ods', 'odt', 'oform', 'otp', 'ots', 'ott', 'oxps', 'pdf', 'pot', 'potm', 'potx', 'pps', 'ppsm', 'ppsx', 'ppt', 'pptm', 'pptx', 'rtf', 'txt', 'xls', 'xlsb', 'xlsm', 'xlsx', 'xlt', 'xltm', 'xltx', 'xml', 'xps'
