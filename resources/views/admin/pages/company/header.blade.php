f8e2a92bb15d18b10b33e44db82501b4300a4195
systemTetra2 <ali.raza03086674345@gmail.com>
2023-03-21 20:59:28 +0500

    verified badge

--- resources/views/admin/pages/company/header.blade.php
+++ resources/views/admin/pages/company/header.blade.php
@@ -6,12 +6,15 @@
       </div>
       <div class="user-profile-header d-flex flex-column flex-sm-row text-sm-start text-center mb-4">
         <div class="flex-shrink-0 mt-n2 mx-sm-0 mx-auto">
-          <img src="{{ asset('assets/img/avatars/14.png') }}" alt="user image" class="d-block h-auto ms-0 ms-sm-4 rounded user-profile-img">
+          <img src="{{ $company->avatar }}" alt="user image" class="d-block h-auto ms-0 ms-sm-4 rounded user-profile-img">
         </div>
         <div class="flex-grow-1 mt-3 mt-sm-5">
           <div class="d-flex align-items-md-end align-items-sm-start align-items-center justify-content-md-between justify-content-start mx-4 flex-md-row flex-column gap-4">
             <div class="user-profile-info">
-              <h4>John Doe</h4>
+              <h4>{{ $company->name }}
+              @if ($company->approval_status == 1)
+                  <img width="30px" src="{{ asset('assets/img/pages/verified.png') }}" alt="">
+              @endif</h4>
               <ul class="list-inline mb-0 d-flex align-items-center flex-wrap justify-content-sm-start justify-content-center gap-2">
                 <li class="list-inline-item">
                   <i class='ti ti-color-swatch'></i> UX Designer
