535b1dcd66fbac1782de30c480ad0e5b1f33243b
systemTetra2 <ali.raza03086674345@gmail.com>
2023-04-04 17:30:08 +0500

    explicitly call seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,5 +24,10 @@ class DatabaseSeeder extends Seeder
         $this->call(AdminRolesPermissionsSeeder::class);
         $this->call(UserRolesPermissionsSeeder::class);
         $this->call(AdminSeeder::class);
+        $this->call(HRSeeder::class);
+        $this->call(CompanySeeder::class);
+        $this->call(ProgramSeeder::class);
+        $this->call(WorkflowSeeder::class);
+        $this->call(KycDocumentSeeder::class);
     }
 }
