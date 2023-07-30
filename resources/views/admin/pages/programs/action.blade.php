40b1af948b9a960757af4968e024619c32e416a1
afifosh <afif@live.com>
2023-07-30 16:06:28 +0300

    update program view action

--- resources/views/admin/pages/programs/action.blade.php
+++ resources/views/admin/pages/programs/action.blade.php
@@ -1,4 +1,6 @@
 <div class="d-inline-block text-nowrap">
+    <a class="btn btn-sm btn-icon" data-bs-toggle="tooltip" aria-label="Preview" data-bs-original-title="Preview" href="{{route('admin.programs.show', $program)}}"><i
+        class="ti ti-eye"></i></a>    
     @can(true)
         <button class="btn btn-sm btn-icon" data-toggle="ajax-modal" data-title="Edit Program" data-href="{{route('admin.programs.edit', $program)}}"><i class="ti ti-edit"></i></button>
     @endcan
@@ -6,12 +8,4 @@
         <button class="btn btn-sm btn-icon delete-record" data-toggle="ajax-delete"
             data-href="{{ route('admin.programs.destroy', $program) }}"><i class="ti ti-trash"></i></button>
     @endcan
-    <button class="btn btn-sm btn-icon dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i
-            class="ti ti-dots-vertical"></i></button>
-    <div class="dropdown-menu dropdown-menu-end m-0">
-        <a href="javascript:;" class="dropdown-item">View</a>
-        @can(true)
-            <a href="javascript:;" class="dropdown-item">Suspend</a>
-        @endcan
-    </div>
 </div>
