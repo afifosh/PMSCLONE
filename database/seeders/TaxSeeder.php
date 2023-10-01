75134ce5ba10d5c71e925fdd885e108c2fbf6369
afifosh <afif@live.com>
2023-10-01 11:22:42 +0300

    Added seeding entries for No Tax, 5% Tax, 15% Tax, and 5% Retention in TaxSeeder

--- database/seeders/TaxSeeder.php
+++ database/seeders/TaxSeeder.php
@@ -12,7 +12,45 @@ class TaxSeeder extends Seeder
    */
   public function run(): void
   {
-    Tax::factory()->count(5)->create();
-    Tax::factory()->count(5)->create(['is_retention' => true]);
+    // Tax::factory()->count(5)->create();
+    // Tax::factory()->count(5)->create(['is_retention' => true]);
+
+        // No Tax 0%
+        Tax::create([
+          'name' => 'No Tax',
+          'type' => 'Percent',
+          'amount' => 0,
+          'status' => 'Active',
+          'is_retention' => false, 
+      ]);
+
+      // 5% Tax
+      Tax::create([
+          'name' => '5% Tax',
+          'type' => 'Percent',
+          'amount' => 5,
+          'status' => 'Active',
+          'is_retention' => false, 
+      ]);
+
+      // 15% Tax
+      Tax::create([
+          'name' => '15% Tax',
+          'type' => 'Percent',
+          'amount' => 15,
+          'status' => 'Active',
+          'is_retention' => false, 
+      ]);    
+
+      // 5% Retention
+      Tax::create([
+        'name' => '5% Retention',
+        'type' => 'Percent',
+        'amount' => 5,
+        'status' => 'Active',
+        'is_retention' => true,  // Assuming this column exists in your Tax model
+      ]);
+
+
   }
 }
