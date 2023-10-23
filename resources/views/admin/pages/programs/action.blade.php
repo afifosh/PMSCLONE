017c4703cbbe4178365f502592aff7cd62f65e37
afifosh <afif@live.com>
2023-10-23 13:07:46 +0300

    update program action

--- resources/views/admin/pages/programs/action.blade.php
+++ resources/views/admin/pages/programs/action.blade.php
@@ -2,10 +2,7 @@
         <button class="btn btn-sm btn-icon" data-toggle="ajax-modal" data-title="Edit Program" data-href="{{route('admin.programs.edit', $program)}}"><i class="ti ti-edit"></i></button>
         <button class="btn btn-sm btn-icon delete-record" data-toggle="ajax-delete"
             data-href="{{ route('admin.programs.destroy', $program) }}"><i class="ti ti-trash"></i></button>
-    <button class="btn btn-sm btn-icon dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i
-            class="ti ti-dots-vertical"></i></button>
-    <div class="dropdown-menu dropdown-menu-end m-0">
-        <a href="javascript:;" class="dropdown-item">View</a>
-            <a href="javascript:;" class="dropdown-item">Suspend</a>
-    </div>
+        <a class="btn btn-sm btn-icon" href="{{ route('admin.programs.show', $program) }}">
+                <i class="ti ti-eye"></i>
+        </a>           
 </div>
