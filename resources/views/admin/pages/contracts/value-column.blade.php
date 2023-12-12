c2062928e543268921841a396a988b79b5094469
Ali Raza <muntazim1001@gmail.com>
2023-12-12 18:51:57 +0500

    contract value breakdown

--- resources/views/admin/pages/contracts/value-column.blade.php
+++ resources/views/admin/pages/contracts/value-column.blade.php
@@ -1,6 +1,6 @@
 <div class="text-start">@cMoney($contract->value ?? 0, $contract->currency, true)
-  {{-- <p class="my-0"><span class="text-warning mt-1">Invoiced:</span> @cMoney($contract->total ?? 0, $contract->currency, true)</p>
+  <p class="my-0"><span class="text-warning mt-1">Invoiced:</span> @cMoney($contract->total ?? 0, $contract->currency, true)</p>
   <div><span class="text-muted">Tax:</span> @cMoney($contract->total_tax ?? 0, $contract->currency, true)</div>
   <div><span class="text-success">Paid:</span> @cMoney($contract->paid_amount ?? 0, $contract->currency, true)</div>
-  <div><span class="text-danger">UnPaid:</span> @cMoney($contract->due_amount ?? 0, $contract->currency, true)</div> --}}
+  <div><span class="text-danger">UnPaid:</span> @cMoney($contract->due_amount ?? 0, $contract->currency, true)</div>
 </div>
